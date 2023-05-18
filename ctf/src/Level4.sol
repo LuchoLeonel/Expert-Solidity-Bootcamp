e87ad535b47593f0a6a846eb6f24b699aa75ff66
LuchoLeonel <carrenoluciano@gmail.com>
2023-05-18 12:32:57 -0300

    wip

--- ctf/src/Level4.sol
+++ ctf/src/Level4.sol
@@ -2,10 +2,18 @@
 pragma solidity 0.8.19;
 
 contract Level_4_Solution {
-        
+    
+    constructor() {
+        assembly {
+            mstore(0x00, 0x600435600355)
+            return(0x1a, 0x06)
+        }
+    }
+/*
     function solution(uint256 value) external {
         assembly {
             sstore(0x03, calldataload(0x04))
         }
     }
+*/
 }
